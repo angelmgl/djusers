1580f75c793ac1d1238a9814b54d893551c079ed
angelmgl <angelemegeele@gmail.com>
2022-09-09 17:17:55 -0400

    Add verification in login form

--- djusers/applications/users/forms.py
+++ djusers/applications/users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import authenticate
 
 from .models import User
 
@@ -8,58 +9,49 @@ class UserRegisterForm(forms.ModelForm):
     custom_password = forms.CharField(
         label="Contraseña",
         required=True,
-        widget=forms.PasswordInput(
-            attrs={
-                'placeholder': 'Introduzca su contraseña'
-            }
-        )
+        widget=forms.PasswordInput(attrs={"placeholder": "Introduzca su contraseña"}),
     )
 
     repeat_password = forms.CharField(
         label="Repetir contraseña",
         required=True,
-        widget=forms.PasswordInput(
-            attrs={
-                'placeholder': 'Repita su contraseña'
-            }
-        )
+        widget=forms.PasswordInput(attrs={"placeholder": "Repita su contraseña"}),
     )
 
     class Meta:
         model = User
-        fields = (
-            'full_name',
-            'email',
-            'username',
-            'gender',
-            'age'
-        )
+        fields = ("full_name", "email", "username", "gender", "age")
 
     def clean_repeat_password(self):
-        if self.cleaned_data['custom_password'] != self.cleaned_data['repeat_password']:
-            self.add_error('repeat_password', 'Las contraseñas no coinciden')
+        if self.cleaned_data["custom_password"] != self.cleaned_data["repeat_password"]:
+            self.add_error("repeat_password", "Las contraseñas no coinciden")
 
-        if len(self.cleaned_data['custom_password']) <= 5:
-            self.add_error('custom_password', 'La contraseña es muy corta')
+        if len(self.cleaned_data["custom_password"]) <= 5:
+            self.add_error("custom_password", "La contraseña es muy corta")
 
 
 class UserLoginForm(forms.Form):
     username = forms.CharField(
         label="Nombre de usuario",
         required=True,
-        widget=forms.TextInput(
-            attrs={
-                'placeholder': 'Introduzca su usuario'
-            }
-        )
+        widget=forms.TextInput(attrs={"placeholder": "Introduzca su usuario"}),
     )
 
     password = forms.CharField(
         label="Contraseña",
         required=True,
-        widget=forms.PasswordInput(
-            attrs={
-                'placeholder': 'Introduzca su contraseña'
-            }
-        )
+        widget=forms.PasswordInput(attrs={"placeholder": "Introduzca su contraseña"}),
     )
+
+    def clean(self):
+        cleaned_data = super(UserLoginForm, self).clean()
+        username = self.cleaned_data["username"]
+        password = self.cleaned_data["password"]
+
+        if not authenticate(
+            username=username,
+            password=password,
+        ):
+            raise forms.ValidationError("Datos incorrectos")
+
+        return self.cleaned_data
